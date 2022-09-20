8a8a409d0a9e437f0f77f74ce0a7ce70ec1cd576
Sado <sadikscorpion@gmail.com>
2022-09-20 18:08:39 +0300

    initial commit

---
+++ Assets/Script/Lessons/ControlRigidbody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Lessons
+{
+    public class ControlRigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private RigidbodySettings _rigidbodySettings;
+
+        private void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(_rigidbodySettings.JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Lessons/RotateSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    [CreateAssetMenu(menuName ="Lessons/Lesson1/Rotate Setings")]
+    public class RotateSettings : ScriptableObject
+    {
+        [SerializeField] private float _speed = 50;
+        public float Speed { get { return _speed; } }
+
+
+    }
+}
